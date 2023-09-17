176e57ec8497a262067e569e2b2b093a5cc126c2
Abood <abood.dar2019@gmail.com>
2023-09-17 20:59:11 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,48 @@
+opening = "Welcome to the calculator"
+print(opening.title())
+
+
+
+def add(x,y) :
+    total1 = x + y
+    return total1
+num1 = int(input('enter number :'))
+num2 = int(input('enter number :'))
+add(num1,num2 )
+print(add(num1,num2))
+
+
+
+
+
+
+def sub(x,y) :
+    total2 = x - y
+    return total2
+num1 = int(input('enter number :'))
+num2 = int(input('enter number :'))
+sub(num1,num2 )
+print(sub(num1,num2))
+
+
+
+
+
+def mul(x,y) :
+    total3 = x * y
+    return total3
+num1 = int(input('enter number :'))
+num2 = int(input('enter number :'))
+mul(num1,num2 )
+print(mul(num1,num2))
+
+
+
+
+def dev(x,y) :
+    total4 = x / y
+    return total4
+num1 = int(input('enter number :'))
+num2 = int(input('enter number :'))
+dev(num1,num2 )
+print(dev(num1,num2))
