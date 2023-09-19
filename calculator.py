d6e6dc8c2db3b64abc5ac6c40bf2b8c6a62f706d
Abood <abood.dar2019@gmail.com>
2023-09-19 14:33:30 +0300

    calculator

--- calculator.py
+++ calculator.py
@@ -1,48 +1,52 @@
-opening = "Welcome to the calculator"
-print(opening.title())
+def calculator() :
+    opening = "Welcome to the calculator"
+    print(opening.title())
 
 
+    print("""
+    1- addition
+    2- subtraction
+    3- multiply
+    4- division""")
 
-def add(x,y) :
-    total1 = x + y
-    return total1
-num1 = int(input('enter number :'))
-num2 = int(input('enter number :'))
-add(num1,num2 )
-print(add(num1,num2))
-
-
-
-
-
-
-def sub(x,y) :
-    total2 = x - y
-    return total2
-num1 = int(input('enter number :'))
-num2 = int(input('enter number :'))
-sub(num1,num2 )
-print(sub(num1,num2))
+    operation = int(input("choose the number of the operation: "))
+    if operation > 4 or operation < 1 :
+        print("number invalid")
+        return
+    num1 = int(input('enter number 1 :'))
+    num2 = int(input('enter number 2 :'))
 
 
+    def add(x,y) :
+        total1 = x + y
+        return total1
 
 
-
-def mul(x,y) :
-    total3 = x * y
-    return total3
-num1 = int(input('enter number :'))
-num2 = int(input('enter number :'))
-mul(num1,num2 )
-print(mul(num1,num2))
+    def sub(x,y) :
+        total2 = x - y
+        return total2
 
 
+    def mul(x,y) :
+        total3 = x * y
+        return total3
 
 
-def dev(x,y) :
-    total4 = x / y
-    return total4
-num1 = int(input('enter number :'))
-num2 = int(input('enter number :'))
-dev(num1,num2 )
-print(dev(num1,num2))
+    def div(x,y) :
+        total4 = x / y
+        return total4
+
+
+    if operation == 1 :
+        print(add(num1, num2))
+    elif operation == 2 :
+        print(sub(num1, num2))
+    elif operation == 3 :
+        print(mul(num1, num2))
+    elif operation == 4 :
+        if num2 == 0 :
+            print("you can't divide by zero")
+            return
+        print(div(num1, num2))
+
+calculator()
